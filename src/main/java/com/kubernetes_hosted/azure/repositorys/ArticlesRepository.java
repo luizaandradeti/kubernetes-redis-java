bda7648ce90f2fd0abf8d335846d21b06752011b
luizaandradeti <197419153+luizaandradeti@users.noreply.github.com>
2025-03-30 19:09:00 -0300

    CODE

--- src/main/java/com/kubernetes_hosted/azure/repositorys/ArticlesRepository.java
+++ src/main/java/com/kubernetes_hosted/azure/repositorys/ArticlesRepository.java
@@ -3,7 +3,6 @@ package com.kubernetes_hosted.azure.repositorys;
 import com.github.javafaker.Faker;
 import com.kubernetes_hosted.azure.entitys.Articles;
 import jakarta.annotation.PostConstruct;
-
 import java.util.ArrayList;
 import java.util.List;
 
